19aa69b1f0046f3f6f6ff8eb10e70e716116675b
waddah-dahi <115942307+waddah-dahi@users.noreply.github.com>
2023-07-18 20:38:22 +0300

    Add files via upload
    An example of arrays in JavaScript

---
+++ example_of_arrays.js
@@ -0,0 +1,31 @@
+// crsPrvdrs = courses providers
+var crsPrvdrs = ["Meta", "Google", "IBM"];
+crsPrvdrs.push("Harvard");
+// crsPrvdrs = crsPrvdrs.join(", ");
+
+// console.log(crsPrvdrs[0]);
+
+// console.log()
+function addCrs(newCrsName, newCrsDscrpt) {
+  var crsName = [newCrsName, newCrsDscrpt];
+  var txtChoose = [
+    'You can choose any "',
+    crsName[0],
+    '" course from the folowing courses:',
+  ];
+  crsName = crsName.join(", ");
+
+  console.log(txtChoose.join(""));
+
+  console.log(" ");
+
+  for (i = 0; i < crsPrvdrs.length; i++) {
+    if (newCrsName != undefined && newCrsDscrpt != undefined) {
+      console.log(crsName, "from " + crsPrvdrs[i]);
+    } else {
+      console.log("Enter both of a name and description for the course.");
+    }
+  }
+}
+addCrs("Front-End Development", "a New course");
+// addCrs();
